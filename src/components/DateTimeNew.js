22f30c1467f53861e622af66ee60bbdc26907ef9
Aleksandr Starkov <77936849+AleksandrStark@users.noreply.github.com>
2021-12-20 16:30:02 +0300

    commit

---
+++ src/components/DateTimeNew.js
@@ -0,0 +1,21 @@
+import DateTime from './DateTime';
+
+export const DateTimeNew = (Component) => (props) => {
+    const hour = 3600 * 1000;
+    const minute = 60 * 1000;
+    const day = 24 * hour;
+    const currentTime = new Date().getTime() - new Date(props.date).getTime();
+    let date;
+
+    if (currentTime > day) {
+        date = `${Math.round(currentTime / day)} дней назад`;
+    } else if (currentTime < hour) {
+        date = `${Math.round(currentTime / minute)} минут назад`;
+    } else if (currentTime >= hour) {
+        date = `${Math.round(currentTime / hour)} часов назад`;
+    }
+
+    return (<Component {...props} date={date} />);
+};
+
+export const NewTime = DateTimeNew(DateTime);
